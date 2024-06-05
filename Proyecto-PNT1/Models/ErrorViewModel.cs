d36ddf23f386dc0e3b3ae58a695b24a7281fd3d8
Matias Esquivel <esquivel.ma90@gmail.com>
2024-06-05 19:39:12 -0300

    Agregar archivos de proyecto.

---
+++ Proyecto-PNT1/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Proyecto_PNT1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
